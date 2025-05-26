b517d868e622488d36983d73792082bd5c02cab6
felipeatari <mr.robot.felipe@gmail.com>
2025-05-26 01:44:47 -0300

    Fix: Add build das imagens

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,6 +6,12 @@ git pull origin main
 echo "==> Parando containers antigos..."
 docker compose down --volumes --remove-orphans
 
+echo "==> Buildando containers (sem cache)..."
+docker compose build --no-cache
+
+echo "==> Limpando imagens não utilizadas atualmente por containers..."
+docker image prune -a --force
+
 echo "==> Subindo containers atualizados..."
 docker compose up -d --remove-orphans
 
