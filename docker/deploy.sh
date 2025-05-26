bfa1f5a840c8e69893b05c88379d04795c1d118e
felipeatari <mr.robot.felipe@gmail.com>
2025-05-26 03:23:10 -0300

    Fix: deploy.sh versão final

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-echo "==> Atualizando repositório..."
+echo ""
+echo "==> 🚀 Atualizando repositório..."
 git pull origin main
 
-echo "==> Gerencia containers..."
+echo ""
+echo "==> 🧹 Limpando containers antigos e preparando ambiente Docker..."
 docker compose down --volumes --remove-orphans
 docker compose build --no-cache
 docker image prune --force
 docker compose up -d --remove-orphans
 
-echo "==> Instala dependencias via composer..."
+echo ""
+echo "==> 📦 Instalando dependências PHP (composer)..."
 docker exec f12-rifas composer install --no-dev --optimize-autoloader
 
-echo "==> Instala dependencias via npm..."
+echo ""
+echo "==> 💻 Instalando dependências JavaScript (npm)..."
 docker exec f12-rifas npm ci
 docker exec f12-rifas npm run build
 docker exec f12-rifas npm prune --omit=dev
 
-echo "==> Instala configurações do laravel..."
+echo ""
+echo "==> 🛠️ Executando migrations..."
 docker exec f12-rifas php artisan migrate --force
-# docker exec f12-rifas php artisan key:generate
 
-echo "==> Limpando caches antigos do Laravel..."
-docker exec f12-rifas php artisan route:clear
-docker exec f12-rifas php artisan config:clear
+echo ""
+echo "==> 🧼 Limpando caches antigos do Laravel..."
 docker exec f12-rifas php artisan cache:clear
+docker exec f12-rifas php artisan config:clear
+docker exec f12-rifas php artisan route:clear
 docker exec f12-rifas php artisan view:clear
 docker exec f12-rifas php artisan optimize:clear
 
-echo "==> Recriando caches e otimizando aplicação..."
+echo ""
+echo "==> 🔧 Recriando caches e otimizando aplicação..."
 docker exec f12-rifas php artisan config:cache
 docker exec f12-rifas php artisan route:cache
 docker exec f12-rifas php artisan view:cache
 docker exec f12-rifas php artisan optimize
 
+echo ""
+echo "✅ Deploy finalizado com sucesso!"
