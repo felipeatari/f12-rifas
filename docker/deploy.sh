0b3c926bb309feee8230cd3000ba310eb2559f39
felipeatari <mr.robot.felipe@gmail.com>
2025-05-26 02:32:58 -0300

    Fix: Rm build no deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,11 +6,11 @@ git pull origin main
 echo "==> Parando containers antigos..."
 docker compose down --volumes --remove-orphans
 
-echo "==> Buildando containers (sem cache)..."
-docker compose build --no-cache
+# echo "==> Buildando containers (sem cache)..."
+# docker compose build --no-cache
 
-echo "==> Limpando imagens não utilizadas atualmente por containers..."
-docker image prune -a --force
+# echo "==> Limpando imagens não utilizadas atualmente por containers..."
+# docker image prune -a --force
 
 echo "==> Subindo containers atualizados..."
 docker compose up -d --remove-orphans
