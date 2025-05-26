46d5a01341561314fb6e8b88e8ebc4cd6dd2c48e
felipeatari <mr.robot.felipe@gmail.com>
2025-05-26 02:58:15 -0300

    Fix: Atualiza deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,16 +3,10 @@
 echo "==> Atualizando repositório..."
 git pull origin main
 
-echo "==> Parando containers antigos..."
+echo "==> Gerencia containers..."
 docker compose down --volumes --remove-orphans
-
-# echo "==> Buildando containers (sem cache)..."
-# docker compose build --no-cache
-
-# echo "==> Limpando imagens não utilizadas atualmente por containers..."
-# docker image prune -a --force
-
-echo "==> Subindo containers atualizados..."
+docker compose build --no-cache
+docker image prune --force
 docker compose up -d --remove-orphans
 
 echo "==> Instala dependencias via composer..."
@@ -20,14 +14,10 @@ docker exec -it f12-rifas composer install --no-dev --optimize-autoloader
 
 echo "==> Instala dependencias via npm..."
 docker exec -it f12-rifas npm ci
-
-echo "==> Builda dependencias via npm..."
 docker exec -it f12-rifas npm run build
-
-echo "==> (Opcional) Remove devDependencies após build..."
 docker exec -it f12-rifas npm prune --omit=dev
 
-echo "==> Executando migrations do Laravel..."
+echo "==> Instala configurações do laravel..."
+docker exec -it f12-rifas php artisan key:generate
 docker exec -it f12-rifas php artisan migrate --force
 
-echo "✅ Deploy finalizado com sucesso!"
