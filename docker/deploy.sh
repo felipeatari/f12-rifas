35e11a46f472d67c2c9a47ccc9739ee36f8c7c22
felipeatari <mr.robot.felipe@gmail.com>
2025-05-26 02:12:29 -0300

    Fix: Roda dependencias no deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -15,8 +15,17 @@ docker image prune -a --force
 echo "==> Subindo containers atualizados..."
 docker compose up -d --remove-orphans
 
-echo "==> Aguardando banco de dados ficar pronto..."
-sleep 30
+# echo "==> Aguardando banco de dados ficar pronto..."
+# sleep 30
+
+echo "==> Instala dependencias via composer..."
+docker exec f12-rifas composer install --no-dev --optimize-autoloader
+
+echo "==> Instala dependencias via npm..."
+docker exec f12-rifas npm ci --omit=dev
+
+echo "==> Builda dependencias via npm..."
+docker exec f12-rifas npm run build
 
 echo "==> Executando migrations do Laravel..."
 docker exec f12-rifas php artisan migrate --force
